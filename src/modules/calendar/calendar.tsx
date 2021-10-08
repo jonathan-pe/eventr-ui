939a1e644e6c20338bd10bc0284dc77fd0eadee1
jonathan-pe <jonathanqpe@gmail.com>
2021-10-08 14:12:25 -0700

    feat: add fullcalendar for calendar page

--- src/modules/calendar/calendar.tsx
+++ src/modules/calendar/calendar.tsx
@@ -1,18 +1,48 @@
 import styled from 'styled-components'
-import { Container, Typography } from '@mui/material'
+import { Box } from '@mui/material'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import list from '@fullcalendar/list'
 
-const StyledContainer = styled(Container)`
+const PageBox = styled(Box)`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: calc(100vh - 64px);
+  min-height: calc(100vh - 64px);
+`
+
+const CalendarBox = styled(Box)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .fc {
+    width: 100%;
+    height: 100%;
+    max-width: 1200px;
+    max-height: 600px;
+  }
 `
 
 const Calendar = (): JSX.Element => {
   return (
-    <StyledContainer>
-      <Typography align='center'>Calendar Page</Typography>
-    </StyledContainer>
+    <PageBox sx={{ mt: 5, mb: 5, ml: 5, mr: 5 }}>
+      <CalendarBox>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, list]}
+          events={[
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+          ]}
+          initialView='dayGridMonth'
+        />
+      </CalendarBox>
+    </PageBox>
   )
 }
 
